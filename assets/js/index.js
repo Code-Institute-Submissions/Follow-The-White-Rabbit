98cf13d03812d028a43ae1818bb368b9f023f3fe
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-06-13 08:28:54 +0000

    Issues with body &main div sizing in mobile view

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 /* Modal loading on page loading 
 The code was taken from https://www.tutorialrepublic.com/codelab.php?topic=faq&file=show-bootstrap-modal-on-page-load */ 
 $(document).ready(function(){
-		$("#myModal").modal('show');
-    });
-
+        $("#myModal").modal('show');
+});
