c6102c3769b9fb4027bbe018709b9c4e3de45b2f
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-06-14 11:56:34 +0000

    Added modal images click closes modal-opens player board-game board

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,54 @@
+
+console.log("test");
+
 /* Modal loading on page loading 
 The code was taken from https://www.tutorialrepublic.com/codelab.php?topic=faq&file=show-bootstrap-modal-on-page-load */ 
-$(document).ready(function(){
-        $("#myModal").modal('show');
+
+
+
+let modal = document.getElementById('myModal');
+let imageE = document.getElementById('imageE');
+let imageM = document.getElementById('imageM');
+let imageH = document.getElementById('imageH');
+let main = document.getElementById('main');
+let playerBoard = document.getElementById('player-board');
+
+$('#imageE').click(function() {
+    easyLevel();
 });
+
+function easyLevel() {
+    $('.modal').addClass('invisible').removeClass('visible');
+    $('#main').addClass('visible').removeClass('invisible');
+    $('#player-board').addClass('visible').removeClass('invisible');
+    $('#game-board-hard').addClass('visible').removeClass('invisible');
+  
+}
+
+$('#imageM').click(function() {
+    mediumLevel();
+});
+
+function mediumLevel() {
+    $('.modal').addClass('invisible').removeClass('visible');
+    $('#main').addClass('visible').removeClass('invisible');
+    $('#player-board').addClass('visible').removeClass('invisible');
+    $('#game-board-hard').addClass('visible').removeClass('invisible');
+  
+}
+$('#imageH').click(function() {
+    hardLevel();
+});
+
+function hardLevel() {
+    $('.modal').addClass('invisible').removeClass('visible');
+    $('#main').addClass('visible').removeClass('invisible');
+    $('#player-board').addClass('visible').removeClass('invisible');
+    $('#game-board-hard').addClass('visible').removeClass('invisible');
+  
+}
+
+
+
+
+
