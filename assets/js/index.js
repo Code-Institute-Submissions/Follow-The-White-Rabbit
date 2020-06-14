5c99463e40064adc2e49794d3f9f6d78f789e342
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-06-14 14:22:50 +0000

    Added difficulty level screens with appropriate number of cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,7 @@ console.log("test");
 /* Modal loading on page loading 
 The code was taken from https://www.tutorialrepublic.com/codelab.php?topic=faq&file=show-bootstrap-modal-on-page-load */ 
 
-
-
+/* Choosing difficulty level and closing the modal   */
 let modal = document.getElementById('myModal');
 let imageE = document.getElementById('imageE');
 let imageM = document.getElementById('imageM');
@@ -17,35 +16,34 @@ $('#imageE').click(function() {
     easyLevel();
 });
 
+$('#imageM').click(function() {
+    mediumLevel();
+});
+
+$('#imageH').click(function() {
+    hardLevel();
+});
+
 function easyLevel() {
     $('.modal').addClass('invisible').removeClass('visible');
     $('#main').addClass('visible').removeClass('invisible');
     $('#player-board').addClass('visible').removeClass('invisible');
-    $('#game-board-hard').addClass('visible').removeClass('invisible');
+    $('#game-board-easy').addClass('visible').removeClass('invisible');
   
 }
-
-$('#imageM').click(function() {
-    mediumLevel();
-});
 
 function mediumLevel() {
     $('.modal').addClass('invisible').removeClass('visible');
     $('#main').addClass('visible').removeClass('invisible');
     $('#player-board').addClass('visible').removeClass('invisible');
-    $('#game-board-hard').addClass('visible').removeClass('invisible');
-  
+    $('#game-board-medium').addClass('visible').removeClass('invisible');
 }
-$('#imageH').click(function() {
-    hardLevel();
-});
 
 function hardLevel() {
     $('.modal').addClass('invisible').removeClass('visible');
     $('#main').addClass('visible').removeClass('invisible');
     $('#player-board').addClass('visible').removeClass('invisible');
     $('#game-board-hard').addClass('visible').removeClass('invisible');
-  
 }
 
 
