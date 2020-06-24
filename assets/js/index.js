5d07b5a80d058189a32961da716e01959896320d
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-06-24 09:15:20 +0000

    Added more pics into random pics

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,15 +33,61 @@ $(document).ready(function(){
 });
 
 /* Array of images from assets/images/random folder  --  index.html div id="card-images" */
+const e = 4;
+const m = 6;
+const h = 9;
 const randomImageArr = ["alice", "cat", "dodo", "caterpillar", "queen", "twins"];
 function setRandomImages() {
-    let num = Math.floor(Math.random() * 6);
+    let randomNum = [];
+    let i = 0;
+    while (i < m) {
+    let num = Math.floor(Math.random() * randomImageArr.length);
+    if (randomNum.indexOf(num) < 0) {
+    randomNum.push(num);
+    }
+    i++;
     console.log(num);
+    console.log(randomNum);
+    }
+
     let img = randomImageArr[ num ];
     console.log(img);
     let imgToPlace = document.getElementById(img);
     document.getElementById("card01").appendChild(imgToPlace); 
     
+}
+
+function randomPics () {
+    let i = 0;
+    let images = [];
+    while (i < 4) {
+        
+        
+        let n = Math.floor(Math.random() * randomImageArr.length);
+        console.log(n);
+        images.push(n);
+        
+        i++;
+    }
+    console.log(images);
+
+    function firstDuplicate() {
+        const dup = {};
+    
+        for(let num of images) {
+
+            if(dup.hasOwnProperty(num)) {
+      
+                 return num;
+                 console.log(num);
+            }
+        }
+    const double = firstDuplicate (images);
+    console.log(double);
+        
+    }
+    
+
 }
 
 
