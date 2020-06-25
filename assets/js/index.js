57a4bcc80c89147b7aca5597faa09e22aab69132
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-06-25 14:18:24 +0000

    Added partial functionality of WhiteRabbit run through game cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,9 +12,6 @@ let modal = document.getElementById('myModal'),
  main = document.getElementById('main'),
  playerBoard = document.getElementById('player-board');
 
-
-
-
 $(document).ready(function(){
     $('#imageE').click(function() {
         $('.modal').hide();
@@ -33,80 +30,108 @@ $(document).ready(function(){
 });
 
 /* Array of images from assets/images/random folder  --  index.html div id="card-images" */
-const e = 4;
-const m = 6;
-const h = 9;
 const easyCards = ["card01","card02","card03","card04"];
 const mediumCards = ["card_1","card_2","card_3","card_4","card_5","card_6"];
 const hardCards = ["card1","card2","card3","card4","card5","card6","card7","card8","card9"];
 const randomImageArr = ["alice", "cat", "dodo", "caterpillar", "queen", "twins", "king", "madhatter", "oysters"];
+const rabbit = ["rabbit"];
+let images = [];
 
+/* Function inseriting random pictures into game cards */
 function randomPics(a,b) {
     let i = 0;
-    let images = [];
+    
     while (i < a) { 
         let n = Math.floor(Math.random() * randomImageArr.length);
         if (images.indexOf(n) < 0) {
         images.push(n);
         i++;
         }
-        console.log(a);
     }
+
     let index = 0;
     while (index < images.length) {
         let img = randomImageArr[ images [index] ];
         let imgToPlace = document.getElementById(img);
         document.getElementById(b[index]).appendChild(imgToPlace); 
+        console.log(imgToPlace);
         index++;
     }
+    return images;
+    
 }
 
-/*
-function randomPics () {
+/* Idea for timer taken from www.w3schools.com  */
+
+
+function rabbitRun(a,b) {
     let i = 0;
-    let images = [];
-    while (i < mediumCards.length) { 
-        let n = Math.floor(Math.random() * randomImageArr.length);
-        if (images.indexOf(n) < 0) {
-        images.push(n);
+    let rabbitRun = [];
+    while (i < a) { 
+        let n = Math.floor(Math.random() * b.length);
+        if (rabbitRun.indexOf(n) < 0) {
+        rabbitRun.push(n);
         i++;
         }
     }
+    
+    console.log("rabbitRun");
+
     let index = 0;
-    while (index < images.length) {
-        let img = randomImageArr[ images [index] ];
-        let imgToPlace = document.getElementById(img);
-        document.getElementById(mediumCards[index]).appendChild(imgToPlace); 
-        index++;
+    while (index < rabbitRun.length) {
+        
+        let imgToPlace = document.getElementById("rabbit");
+        let card = document.getElementById(b [rabbitRun [index] ] );
+        console.log(rabbitRun[index], card);        console.log(rabbitRun);
+        let img = rabbitRun [index] ;
+        let imageToRemove = document.getElementById(randomImageArr [images[img]]);
+        let imageToPlace = document.getElementById(rabbit[0]);
+        console.log(images);         console.log(img);         console.log(card, imageToRemove, imageToPlace);
+        
+        card.replaceChild(imageToPlace, imageToRemove);
+        card.replaceChild(imageToRemove, imageToPlace);
+        index+1;
+card.replaceChild(imageToPlace, imageToRemove);
+        card.replaceChild(imageToRemove, imageToPlace);
+        index+1;
+        
+
+        
+        
+                
+           
+        
     }
+
+    function replaceImage(card, imageToPlace, imageToRemove) {
+        setTimeout(function() { 
+            card.replaceChild(imageToPlace, imageToRemove);
+            card.replaceChild(imageToRemove, imageToPlace);
+
+            }, 1000 * index); 
+    }
+
+
+    console.log(rabbitRun);
+    return rabbitRun;
 }
 
-function randomPics2 () {
-    let i = 0;
-    let images = [];
-    while (i < hardCards.length) { 
-        let n = Math.floor(Math.random() * randomImageArr.length);
-        if (images.indexOf(n) < 0) {
-        images.push(n);
-        i++;
-        }
-    }
+
+    
+    /*
     let index = 0;
-    while (index < images.length) {
-        let img = randomImageArr[ images [index] ];
-        let imgToPlace = document.getElementById(img);
-        document.getElementById(hardCards[index]).appendChild(imgToPlace); 
+    while (index < rabbitRun.length) {
+        let imgToPlace = document.getElementById("rabbit");
+        console.log(imgToPlace, images[index]);
+        
+        document.getElementById(rabbitRun[index]).removeChild(images[index]).appendChild(imgToPlace);  
+        console.log(rabbitRun[index]);
+        console.log("test");
         index++;
     }
-}
+    console.log(rabbitRun);
+}  
 */
+    
 
-
-
-
-
-
-
-
-
-
+console.log();
