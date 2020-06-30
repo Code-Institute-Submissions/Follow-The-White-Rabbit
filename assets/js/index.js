b143242638b675a668f8bd24b31ccfca44a96570
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-06-30 11:16:14 +0000

    Added image clicks capture + comparison to RabbiRun on all difficulty levels

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,45 +34,54 @@ const easyCards = ["card01","card02","card03","card04"];
 const mediumCards = ["card_1","card_2","card_3","card_4","card_5","card_6"];
 const hardCards = ["card1","card2","card3","card4","card5","card6","card7","card8","card9"];
 const randomImageArr = ["alice", "cat", "dodo", "caterpillar", "queen", "twins", "king", "madhatter", "oysters"];
-const rabbit = ["rabbit"];
-let images = [];
 
+
+
+var images = [];
 /* Function inseriting random pictures into game cards */
-
-// document.getElementsByClassName(".level-card").addEventListener("click", randomPics);
-
 function randomPics(a,b) {
-    let i = 0;
     
+    let i = 0;
+    console.log("from randomPics 1: " + images)
     while (i < a) { 
         let n = Math.floor(Math.random() * randomImageArr.length);
+        console.log("n: " + n);
         if (images.indexOf(n) < 0) {
         images.push(n);
         i++;
         }
+        console.log("from randomPics 2: " + images)
     }
-
+    console.log("from randomPics 3: " + images)
     let index = 0;
     while (index < images.length) {
         let img = randomImageArr[ images [index] ];
         let imgToPlace = document.getElementById(img);
+        console.log(b[index]);
         document.getElementById(b[index]).appendChild(imgToPlace); 
         index++;
     }
-    console.log("images:" + images)
+}
+
+function reset(b){
+    console.log("reset");
+    let index = 0;
+    while (index < images.length) {
+        let img = randomImageArr[ images [index] ];
+        let imgToPlace = document.getElementById(img);
+        document.getElementById(b[index]).removeChild(imgToPlace); 
+        index++;
+    }
+    images.length = 0;
+    console.log("from reset:" + images); 
+    
+    return randomPics();
     return images;
 }
 
-<<<<<<< HEAD
-function rabbitRun(a,b,run) {
-=======
-
-/* Idea for timer taken from www.w3schools.com  */
 let rabbitRun = [];
 function whiteRabbitRun(a,b) {
->>>>>>> origin/rabbitrun
     let i = 0;
-    
     while (i < a) { 
         let n = Math.floor(Math.random() * b.length);
         if (rabbitRun.indexOf(n) < 0) {
@@ -80,127 +89,51 @@ function whiteRabbitRun(a,b) {
         i++;
         }
     }
+        console.log("rabbitRun: " + rabbitRun);
     
-    console.log("rabbitRun: " + rabbitRun);
-
-<<<<<<< HEAD
-    
-    let index = 0;
-    while (index < rabbitRun.length) {
-        let card = document.getElementById(b [rabbitRun [index] ] );
-        console.log(rabbitRun[index], card);        console.log(rabbitRun);
-        let img = rabbitRun [index] ;
-        let imageToRemove = document.getElementById(randomImageArr [images[img]]);
-        let imageToPlace = document.getElementById(rabbit[0]);
-        console.log(images);         console.log(img);         console.log(card, imageToRemove, imageToPlace);
-       
-            console.log(card);
-            card.removeChild(imageToRemove);
-            card.appendChild(imageToPlace);
-        setTimeout(() => { 
-            console.log(card);
-            card.appendChild(imageToRemove);
-            card.removeChild(imageToPlace);
-            
-            
-        }, 500);  
-        
-        
-
-        index++;
-    }    
-    
-
-         
-
-
-    
-=======
-    let index = 0;
-    let rabbitImg = $("#rabbit");
-    let card = b[rabbitRun[index]];  
-    
-    $("#" + card).find("img:first").hide(); // hide the first image (ie: Alice)
-        //debugger; // turn-on to see it work "in action"
-
-    $("#" + card).append($(rabbitImg)).show(); // append White Rabbit image and how him
-        //debugger; // turn-on to see it work "in action"
->>>>>>> origin/rabbitrun
-
-    //$("#" + card).find("img:last").remove(); // find White Rabbit image and remove him
-        //debugger; // turn-on to see it work "in action"
-
-    // $("#" + card).find("img:first").show(); // find first image and show it (ie: Alice)
-        //debugger; // turn-on to see it work "in acti
-    
+    /* START - Code here was written with help from tutors: Stephen & Tim  */
+    let index = 0, 
+     rabbitImg = $("#rabbit"),
+     card = b[rabbitRun[index]];  
+        $("#" + card).find("img:first").hide(); // hide the first image 
+        $("#" + card).append($(rabbitImg)).show(); // append White Rabbit image and how him
     var myInterval = setInterval(function(){ 
         $("#" + card).find("img:last").remove(); // find White Rabbit image and remove him
-        //debugger; // turn-on to see it work "in action"
-
-        $("#" + card).find("img:first").show(); // find first image and show it (ie: Alice)
-        //debugger; // turn-on to see it work "in acti
-        index++;
-         card = b[rabbitRun[index]];
-       $("#" + card).find("img:first").hide(); // hide the first image (ie: Alice)
-        //debugger; // turn-on to see it work "in action"
-
-       $("#" + card).append($(rabbitImg)).show(); // append White Rabbit image and how him
-        //debugger; // turn-on to see it work "in action"
+        $("#" + card).find("img:first").show(); // find first image and show it 
+            index++;
+            card = b[rabbitRun[index]];
+        $("#" + card).find("img:first").hide(); // hide the first image
+        $("#" + card).append($(rabbitImg)).show(); // append White Rabbit image and hide it
         
-        if  (index >= rabbitRun.length) {
+       if  (index >= rabbitRun.length) {
             clearInterval(myInterval);
-        }
-       // $("#" + card).find("img:last").remove(); // find White Rabbit image and remove him
-        //debugger; // turn-on to see it work "in action"
-
-        //$("#" + card).find("img:first").show(); // find first image and show it (ie: Alice)
-        //debugger; // turn-on to see it work "in acti
-        
-    }, 1000);
+        }      
+    }, 500);
+    return rabbitRun;
     
-<<<<<<< HEAD
-    
-=======
 }
-   // while (index < rabbitRun.length) {
-        
-        // let img = rabbitRun[index];
-        // let imageToRemove = document.getElementById(randomImageArr [images[img]]);
-        // let imageToPlace = document.getElementById(rabbit[0]);
-/*
-        $("#" + card).find("img:first").hide(); // hide the first image (ie: Alice)
-        debugger; // turn-on to see it work "in action"
+   /* finish - Code here was writtne with help from tutors: Stephen & Tim  */
 
-        $("#" + card).append($(rabbitImg)).show(); // append White Rabbit image and how him
-        debugger; // turn-on to see it work "in action"
 
-        $("#" + card).find("img:last").remove(); // find White Rabbit image and remove him
-        debugger; // turn-on to see it work "in action"
+let board;
+$(".board").on("click", function() {    
+    let boardId = $(this).attr("id");;
+    if (boardId == "game-board-easy") board = easyCards;
+    if (boardId == "game-board-medium") board = mediumCards;
+    if (boardId == "game-board-hard") board = hardCards;
+    console.log(board);
+});
 
-        $("#" + card).find("img:first").show(); // find first image and show it (ie: Alice)
-        debugger; // turn-on to see it work "in action"
-
-        index++;
-    }    
-    */
-    console.log("function done: " + rabbitRun);
-    //return rabbitRun;
 
 
 /* Capturing players' clicks through game cards  */
 let followRabbit = []; // defined globally, not in a function
 $(".game-card").on("click", function() {     
-    if (followRabbit.length === images.length) {
+    if (followRabbit.length === images.length) 
         return followRabbit;
-    } 
-    else {
-    gameCardID = $(this).attr("id");
-    //index = $(".board").indexOf(gameCardID);
-    index = easyCards.indexOf(gameCardID);
-    followRabbit.push(index);
-    }    
-    console.log("rabbitRun: " + rabbitRun.length + ": " + rabbitRun);
-    console.log("followRabbit: " +followRabbit.length + ": " + followRabbit);
+    else 
+        index = board.indexOf($(this).attr("id"));
+        followRabbit.push(index);
 });
 
 
@@ -219,5 +152,4 @@ $(".game-card").on("click", function() {
 });
 
 
->>>>>>> origin/rabbitrun
 
